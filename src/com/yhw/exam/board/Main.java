75740887af800b0d01027d19103303ada25b74de
yhw0536 <yhw0536@gmail.com>
2021-11-16 12:39:25 +0900

    세팅

---
+++ src/com/yhw/exam/board/Main.java
@@ -0,0 +1,12 @@
+package com.yhw.exam.board;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // write your code here
+    System.out.println("== 게시판 v 0.1 ==");
+    System.out.println("== 프로그램 시작 ==");
+    System.out.println("명령)");
+    System.out.println("== 프로그램 종료 ==");
+  }
+}
