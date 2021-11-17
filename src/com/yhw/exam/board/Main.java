4c2cafbc67f91bc001fcf9a4b0723a708c4bebb9
yhw0536 <yhw0536@gmail.com>
2021-11-17 10:20:30 +0900

    게시물 번호가 매번 증가

--- src/com/yhw/exam/board/Main.java
+++ src/com/yhw/exam/board/Main.java
@@ -9,6 +9,8 @@ public class Main {
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
+
+    int articlesLastId = 0;
     while (true) {
       System.out.printf("명령 : ");
       String str = sc.nextLine();
@@ -19,10 +21,12 @@ public class Main {
         String title = sc.nextLine();
         System.out.printf("내용 : ");
         String board = sc.nextLine();
-        int Num = 1;
+        int Num = articlesLastId + 1;
+        articlesLastId = Num;
+
         System.out.printf("%d번 게시물이 등록되었습니다. \n", Num);
-      }
-      else if (str.equals("exit")) {
+
+      } else if (str.equals("exit")) {
         break;
       }
     }
