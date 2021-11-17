bc5366be0005c050a9fdab01f1d9d5a3ec5638af
yhw0536 <yhw0536@gmail.com>
2021-11-17 10:58:44 +0900

    toString 오버라이딩

--- src/com/yhw/exam/board/Main.java
+++ src/com/yhw/exam/board/Main.java
@@ -2,10 +2,16 @@ package com.yhw.exam.board;
 
 import java.util.Scanner;
 
-class Article {
+class Article extends Object {
   int id;
   String title;
   String body;
+
+  @Override
+  public String toString() {
+    // return String.format("{id: " + id + ", " + "title: " + title + ", " + "body: " + body + "}");
+    return String.format("{id : %d, title : \"%s\" body : \"%s\"}", id, title, body);
+  }
 }
 
 public class Main {
@@ -33,10 +39,16 @@ public class Main {
         article.id = id;
         article.title = title;
         article.body = body;
-        System.out.println("생성된 게시물 객체 : " + article);
-
+        System.out.println("생성된 게시물 객체 : " + article.toString());
         System.out.printf("%d번 게시물이 등록되었습니다. \n", id);
 
+        /*
+        System.out.println("id: " + id);
+        System.out.println("title : " + title);
+        System.out.println("body : " + body);
+        */
+
+        System.out.println();
 
       } else if (str.equals("exit")) {
         break;
